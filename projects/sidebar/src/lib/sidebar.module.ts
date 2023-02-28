c0bcd3aa18a9787a7bce8d123675dd614c35833d
Tsvetkov, Kaloyan <kaloyan.tsvetkov.sp@lht.dlh.de>
2023-02-28 14:39:40 +0200

    downgraded angular version to 13.3.0 and generated the library

---
+++ projects/sidebar/src/lib/sidebar.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { SidebarComponent } from './sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidebarComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    SidebarComponent
+  ]
+})
+export class SidebarModule { }
